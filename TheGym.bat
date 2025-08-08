393b221002fdbb9b3a6aeda57ce43579b9b0aacf
Moataz0000 <moatazfawzy150@gmail.com>
2025-08-08 14:23:01 +0300

    feat

--- TheGym.bat
+++ TheGym.bat
@@ -1,18 +1,14 @@
 @echo off
-REM ✅ الانتقال لمكان السكريبت
-cd /d %~dp0
+REM Navigate to your Django project folder
+cd dev\mycoffee
 
-REM ✅ تفعيل البيئة الافتراضية
-call .venv\Scripts\activate.bat
+REM Activate the virtual environment
+call .\venv\Scripts\activate
 
-REM ✅ دخول مجلد config (لو manage.py فيه)
-cd config
+REM Open the browser at localhost
+start http://localhost:8000
 
-REM ✅ تشغيل السيرفر في الخلفية
-start "" python manage.py runserver
+REM Run Django development server
+python manage.py runserver
 
-REM ✅ الانتظار ثانيتين
-timeout /t 2 >nul
-
-REM ✅ فتح المتصفح Google Chrome على صفحة الأدمن
-start chrome http://127.0.0.1:8000/admin/
+pause
